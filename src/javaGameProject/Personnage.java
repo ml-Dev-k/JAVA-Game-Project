24e0fb27eb3ad54b5845a5adef26614baea876d7
ml-Dev-K <kanemouhamadoulamine50@gmail.com>
2025-02-22 00:17:49 +0100

    initialisation du projet

---
+++ src/javaGameProject/Personnage.java
@@ -0,0 +1,10 @@
+package javaGameProject;
+
+public class Personnage {
+    private String nom;
+    private String nomImage;
+    private String[] dialogue;
+    private String description;
+    private Object[] listeObjet;
+
+}
